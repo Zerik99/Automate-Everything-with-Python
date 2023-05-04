20b6de6496f5969ef35c8a1873d2594368bb4ff1
Erik Zander <erikzndr@gmail.com>
2023-05-04 12:42:58 -0500

    removed while loop as this made the code messy and realistically we would usa  scheduler to run this task.

--- Project - Stock Price Notifier/stock_price_notification_app.py
+++ Project - Stock Price Notifier/stock_price_notification_app.py
@@ -1,5 +1,6 @@
 import os
 import yagmail
+from selenium import webdriver
 import SeleniumWebDriverClass as swdc
 from time import sleep
 
@@ -10,35 +11,28 @@ from time import sleep
 The app uses zse.hr to get the stock price data."""
 
 
-def main(stockPriceChange: float = 0.0, notifyCoolDown: int = 0) -> None:
-    while True:
-        # get stock price change percent
-        try:
-            stockPriceChange = get_stock_price_change_percent()
-        except Exception as e:
-            print(type(e))
-            sleep(5)
-            continue
+def main(url) -> None:
+    # create a webdriver object
+    driver = create_webdriver(url=url)
 
-        # send notification if stock price change is less than -0.10%
-        if stockPriceChange > -0.05 or notifyCoolDown > 0:
-            print("No Notification Sent! requirements not met.")
-            notifyCoolDown -= 1
-            sleep(5)
-            continue
-        send_email_notification(stockPriceChange)
-        notifyCoolDown = 2
-        sleep(5)
+    # get stock price change percent
+    stockPriceChange = get_stock_price_change_percent(driver=driver)
+
+    # send notification if stock price change is less than -0.10%
+    if stockPriceChange > -0.05:
+        print("No Notification Sent!")
+    Status = send_email_notification(stockPriceChange)
+    print(Status)
 
 
-def get_stock_price_change_percent() -> float:
-    url = "https://zse.hr/en/indeks-366/365?isin=HRZB00ICBEX6"
-    xpath = "/html/body/div[2]/div/section[1]/div/div/div[2]/span[2]"
-
+def create_webdriver(url) -> webdriver.Chrome:
     myWebDriver = swdc.WebDriverTemplate(siteURL=url)
     driver = myWebDriver.get_driver()
-    sleep(3)
+    return driver
 
+
+def get_stock_price_change_percent(driver: webdriver.Chrome) -> float:
+    xpath = "/html/body/div[2]/div/section[1]/div/div/div[2]/span[2]"
     stockPriceChangePercent = driver.find_element(by="xpath", value=xpath).text
     driver.quit()
 
@@ -46,7 +40,7 @@ def get_stock_price_change_percent() -> float:
     return float(stockPriceChangePercent.replace("%", ""))
 
 
-def send_email_notification(spChange: float) -> None:
+def send_email_notification(spChange: float) -> str:
     sender = os.environ.get("Py_Automation_Gmail")
     senderpw = os.environ.get("Py_Automation_Gmail_App_Pw")
     receiver = os.environ.get("Personal_Gmail")
@@ -57,9 +51,16 @@ def send_email_notification(spChange: float) -> None:
     The Stock price is down {spChange}%. """
 
     yag = yagmail.SMTP(sender, senderpw)
-    yag.send(to=receiver, subject=subject, contents=contents)
-    print("Email Sent!")
+    try:
+        yag.send(to=receiver, subject=subject, contents=contents)
+    except Exception as e:
+        status = f"Email not sent! Error type: {type(e)} "
+        return status
+    else:
+        status = "Email sent successfully!"
+        return status
 
 
 if __name__ == "__main__":
-    main()
+    _url = "https://zse.hr/en/indeks-366/365?isin=HRZB00ICBEX6"
+    main(_url)
