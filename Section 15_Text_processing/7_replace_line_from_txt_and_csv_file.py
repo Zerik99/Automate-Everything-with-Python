22ac620e155f3c6e19e360d8fb9bc84b800addef
Erik Zander <erikzndr@gmail.com>
2023-05-20 18:19:56 -0500

    test

--- Section 15_Text_processing/7_replace_line_from_txt_and_csv_file.py
+++ Section 15_Text_processing/7_replace_line_from_txt_and_csv_file.py
@@ -25,3 +25,24 @@ for i, filepath in enumerate(files_dir.iterdir()):
 
     with open(f"{filesbackup_dir}/merged_data{f_ext}", "w", encoding="utf-8") as file:
         file.write(merged_content)
+
+# -----logic documentation----- #
+# 1. Create a files directory and a filesbackup directory.
+# 2. Create a variable to store the merged content.
+# 3. Create a variable to store the content of the file as a string.
+# 4. Iterate over each file in the files directory.
+# 5. Open the file in read mode.
+# 6. Create a variable to store the content of the file as a list.
+# 7. Create a variable to store the content of the file as a list without the header.
+# 8. If the file is the first file in the directory:
+#       - Create a variable to store the content of the file as a string with a header.
+#       - Add the content of the file as a string with a header to the merged content.
+#       - Close the file.
+# 9. Otherwise:
+#       - Create a variable to store the content of the file as a string without a header.
+#       - Add the content of the file as a string without a header to the merged content.
+#       - Close the file.
+# 10. Create a variable to store the extension of the file.
+# 11. Open a file in the filesbackup directory in write mode.
+# 12. Write the merged content to the file.
+# 13. Close the file.
