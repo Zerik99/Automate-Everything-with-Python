ba06f0eb0534f7ac7754185f7d4c2bd05f450ad8
Erik Zander <erikzndr@gmail.com>
2023-05-23 09:53:36 -0500

    saved changes to section 17 lecture 4 sentiment analysis

--- Section 17_Natural_language_Processing/4_sentiment_analysis_mood_analyzer.py
+++ Section 17_Natural_language_Processing/4_sentiment_analysis_mood_analyzer.py
@@ -1,5 +1,5 @@
+"""a simple app to demonstrate sentiment analysis using NLTK."""
 import nltk
-
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 analyzer = SentimentIntensityAnalyzer()
@@ -13,7 +13,7 @@ twitter_sentiment: dict[str, list[int]] = {"pos": [0], "neg": [0], "neu": [0]}
 
 # for each tweet in twitter_samples increase counter for positive, negative or neutral sentiment.
 for tweet in twitter_samples:
-    sentiment = analyzer.polarity_scores(tweet)
+    sentiment: dict[str, float] = analyzer.polarity_scores(tweet)
     if sentiment["compound"] > 0:
         twitter_sentiment["pos"][0] += 1
         print("Positive")
